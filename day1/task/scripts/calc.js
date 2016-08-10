d3fdb3d54e50e7afc010405e094b1e94d5b3f658
Sergu <sergushov.1995@mail.ru>
2016-08-10 21:16:42 +0300

    bugs fixed

--- day1/task/scripts/calc.js
+++ day1/task/scripts/calc.js
@@ -1,22 +1,18 @@
 
 for (var i = 0; i <= data.length -1; i++) {
 
-	if((!isNaN(data[i])) && (data[i] != null)){
+	if(!isNaN(data[i]) && (data[i] != null)){
 		if((data[i] < 100) && (data[i] != 0)){
 				data[i] = Number(data[i]) + 100;
 		}
-		else
+		else if(data[i] == 0)
 		{
-			if(data[i] == 0){
-				data[i] = Number(data[i]) + 10;
-			}
-			else{
-				if(data[i] > 100){
-					data[i] = data[i] - 100;
-				}
-			}
+			data[i] = Number(data[i]) + 10;
+		}
+		else if(data[i] > 100){
+			data[i] = data[i] - 100;
 		}
 	}
 };
 
-OutputArray(data);
+OutputArray();
--- day1/task/scripts/log.js
+++ day1/task/scripts/log.js
@@ -1,17 +1,15 @@
 
-function OutputArray(array){
-	for (var i = 0; i <= array.length -1; i++) 
+function OutputArray(){
+	for (var i = 0; i <= data.length -1; i++) 
 	{
-		if(typeof array[i] == "undefined"){
-			console.log("array[{"+ i +"}]={"+ "не определено" +"}")
+		if(typeof data[i] == "undefined"){
+			console.log("data[{%d}]={%s}",i,"не определено")
+		}
+		else if(data[i] == null){
+			console.log("data[{%d}]={%s}",i,"не указано")
 		}
 		else{
-			if(array[i] == null){
-				console.log("array[{"+ i +"}]={"+ "не указано" +"}")
-			}
-			else{
-				console.log("array[{"+ i +"}]={"+ array[i] +"}")
-			}
+			console.log("data[{%d}]={%s}",i,data[i])
 		}
 	};
 }
