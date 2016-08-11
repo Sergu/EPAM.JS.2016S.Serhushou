c0fa4ffb26f441b8e4194cbb3af834d1c7b3bfdc
Sergu <sergushov.1995@mail.ru>
2016-08-11 18:03:39 +0300

    day2 implimented

---
+++ day2/task/scripts/log.js
@@ -0,0 +1,26 @@
+var typeCount = [];																// массив хранения результатов суммы классов
+typeCount[1] = 0;																// инициализация суммы каждого класса нулями
+typeCount[2] = 0;
+typeCount[3] = 0;
+
+for (var i = 0; i < arr.length; i++) {											// проход по всем элементам массива элементов
+	switch(arr[i].type){							
+		case 1:
+			var a = arr[i].getCount1();
+			typeCount[1] = typeCount[1] + arr[i].getCount1();					// в зависимости от значения свойства type, определяется
+																				// нужный метод, который надо вызвать для получения значения
+			break;
+		case 2:
+			var a = arr[i].getCount2();
+			typeCount[2] = typeCount[2] + arr[i].getCount2();
+			break;
+		case 3:
+			var a = arr[i].getCount3();
+			typeCount[3] = typeCount[3] + arr[i].getCount3();
+			break;
+	}
+};
+
+for (var i = 1; i < typeCount.length; i++) {									// вывод суммы всех классов
+	console.log("count{%d}={%d}",i,typeCount[i])
+};
