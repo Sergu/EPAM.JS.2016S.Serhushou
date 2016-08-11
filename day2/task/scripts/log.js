a6d0f3845d198060e1e4ab5c557695fc263dfaf8
Sergu <sergushov.1995@mail.ru>
2016-08-11 18:25:19 +0300

    log.js  func call with dynamic name

--- day2/task/scripts/log.js
+++ day2/task/scripts/log.js
@@ -4,21 +4,9 @@ typeCount[2] = 0;
 typeCount[3] = 0;
 
 for (var i = 0; i < arr.length; i++) {		// проход по всем элементам массива элементов
-	switch(arr[i].type){							
-		case 1:
-			var a = arr[i].getCount1();
-			typeCount[1] = typeCount[1] + arr[i].getCount1();// в зависимости от значения свойства type, определяется
-															// нужный метод, который надо вызвать для получения значения
-			break;
-		case 2:
-			var a = arr[i].getCount2();
-			typeCount[2] = typeCount[2] + arr[i].getCount2();
-			break;
-		case 3:
-			var a = arr[i].getCount3();
-			typeCount[3] = typeCount[3] + arr[i].getCount3();
-			break;
-	}
+	var currentObj = arr[i];
+	var type = currentObj.type;
+	typeCount[type] += currentObj["getCount" + type]();
 };
 
 for (var i = 1; i < typeCount.length; i++) {	// вывод суммы всех классов
