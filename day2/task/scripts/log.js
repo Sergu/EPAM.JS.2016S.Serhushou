968d375e6c73a7f142b0ab85b75307a8a5705332
Sergu <sergushov.1995@mail.ru>
2016-08-11 18:30:14 +0300

    log.js func call with dynamic name , comments

--- day2/task/scripts/log.js
+++ day2/task/scripts/log.js
@@ -6,7 +6,7 @@ typeCount[3] = 0;
 for (var i = 0; i < arr.length; i++) {		// проход по всем элементам массива элементов
 	var currentObj = arr[i];
 	var type = currentObj.type;
-	typeCount[type] += currentObj["getCount" + type]();
+	typeCount[type] += currentObj["getCount" + type]();	// вызов метода объекта по имени используя конкатенацию "getCount" и типа объекта
 };
 
 for (var i = 1; i < typeCount.length; i++) {	// вывод суммы всех классов
