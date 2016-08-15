7a0f5ad05d67393f5d4a53a7ae58feeddd6bf6bc
Sergu <sergush.nik@mail.ru>
2016-08-15 10:36:58 +0300

    switch removed

--- day2/task/scripts/init.js
+++ day2/task/scripts/init.js
@@ -1,40 +1,14 @@
 var arr = [];
 
+
 for ( var i = 0; i < 5; i++) {
 	var objType = random(1,3);  // опредение типа объекта
-	var obj;
-	switch (objType) {															
-		case 1:
-			obj = {		// создание обекта определённого типа
-				count: random(1,10),
-				type: 1,	// свойство типа объекта, нужно для того , чтобы понять
-							// какой метод нужно вызывать на объекте
-				getCount1: function(){
-					return this.count;
-				}
-			}
-			console.log("type={%d},count{%d}",obj.type,obj.getCount1())	// вывод добавляемого элемента
-			break;
-		case 2:
-			obj = {
-				count: random(1,10),
-				type: 2,
-				getCount2: function(){
-					return this.count;
-				}
-			}
-			console.log("type={%d},count{%d}",obj.type,obj.getCount2())
-			break;
-		case 3:
-			obj = {
-				count: random(1,10),
-				type: 3,
-				getCount3: function(){
-					return this.count;
-				}
-			}
-			console.log("type={%d},count{%d}",obj.type,obj.getCount3())
-			break;
-	}
+	var obj = {};
+	
+	obj.count = random(1,10);
+	obj["getCount"+objType] = function() { return this.count}
+	
+	console.log("type={%d},count{%d}",objType,obj["getCount"+objType].call(obj))
+	
 	arr[i] = obj;
 }
