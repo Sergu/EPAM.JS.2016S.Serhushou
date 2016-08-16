dfd6d09ed631435dac50fb50af2b1c8aa5aaf0d9
Sergu <sergushov.1995@mail.ru>
2016-08-16 20:00:01 +0300

    day3 realized

--- day3/task/js/scripts.js
+++ day3/task/js/scripts.js
@@ -1,3 +1,66 @@
-function(){
-	
+$(function(){
+	var generateButton = $("#generate");
+	generateButton.click(generate);
+	releaseElement(generateButton);
+
+	var resetButton = $("#reset");
+	resetButton.click(reset);
+	blockElement(resetButton);
+
+	var colorButton = $("#color");
+	colorButton.click(setColor);
+	blockElement(colorButton);
+
+});
+
+
+function generate(){
+	var container = $(".field-container")
+	for (var i = 0; i < 50; i++) {
+		var field = $("<div></div>").text(random(1,100));
+		field.addClass("field");
+		field.addClass("pointer-cursor");
+		container.append(field);
+	};
+	blockElement($("#generate"));
+	releaseElement($("#color"));
+	releaseElement($("#reset"));
+}
+
+function setColor(){
+	var fields = $(".field").each(function(){
+		var numb = $(this).text();	
+		if (numb > 75) {
+			$(this).addClass("red-field");
+		}
+		else if (numb > 50) {
+			$(this).addClass("orange-field");
+		}
+		else if(numb > 25) {
+			$(this).addClass("green-field");
+		}
+	});
+	blockElement($("#color"));
+}
+
+function reset(){
+	var container = $(".field-container");
+	container.empty();
+	blockElement($("#reset"));
+	releaseElement($("#generate"));
+	blockElement($("#color"));
+}
+
+function blockElement(element){
+	element.removeClass("released-div");
+	element.addClass("blocked-div");
+}
+
+function releaseElement(element){
+	element.removeClass("blocked-div");
+	element.addClass("released-div");
+}
+
+function random(min, max) {
+	return Math.floor((Math.random() * (max - min + 1)) + min);
 }
