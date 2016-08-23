bf9689acd13652778f56b1f92e78e3594c383853
Sergu <sergush.nik@mail.ru>
2016-08-23 12:39:35 +0300

    healthy zombies

--- day4/task/js/zombies.js
+++ day4/task/js/zombies.js
@@ -1,7 +1,6 @@
 function ZombieMickael(id,road){
 	Zombie.apply(this,arguments);
 	
-	this.HEALTH = 1000;
 	this.SPEED = 13000;
 
 	this.zombieHtml.addClass("zombie michael");
@@ -10,25 +9,44 @@ function ZombieMickael(id,road){
 function ZombieStrong(id,road){
 	Zombie.apply(this,arguments);
 	
-	this.HEALTH = 1000;
-	this.SPEED = 17000;
+	this.HEALTH = 100;
+	this.MAX_HEALTH = 100;
+	this.SPEED = 15000;
 
 	this.zombieHtml.addClass("zombie strong");
 }
 
 function Zombie(id,road){
+	
 	this.id = id;
 	this.road = road;
+	
+	this.HEALTH = 70;
+	this.MAX_HEALTH = 70;
 
+	this.zombieField = $("<div></div>");
+	this.zombieField.addClass("zombie-field");
+	
 	this.zombieHtml = $("<div></div>");
-
-	this.road.append(this.zombieHtml);
+	
+	this.healthHtml = $("<div></div>");
+	this.healthHtml.addClass("health");
+	
+	this.zombieField.append(this.healthHtml);
+	this.zombieField.append(this.zombieHtml);
+	this.road.append(this.zombieField);
 
 	this.move = function(){
 
-		var position = this.zombieHtml.position().left;
+		var position = this.zombieField.position().left;
+		var width = this.zombieField.width();
 		if(position != 0) {
-			this.zombieHtml.animate({left: 0},this.SPEED,'linear');
+			this.zombieField.animate({left:0,width: this.zombieHtml.width() + 'px'},this.SPEED,'linear');
+			var isHealthy = this.checkHealth();
+			if(isHealthy == "dead"){
+				this.stop();
+				this.remove();
+			}
 			return "walking";
 		}
 		else{
@@ -36,10 +54,21 @@ function Zombie(id,road){
 			return "finished";
 		}
 	};
+	
+	this.checkHealth = function(){
+		var healthPersentile = this.HEALTH/this.MAX_HEALTH * 100;
+		this.healthHtml.text(this.HEALTH);
+		
+		if(this.health == 0)
+			return "dead";
+		else
+			return "healthy";
+	}
+	
 	this.stop = function(){
-		this.zombieHtml.stop();
+		this.zombieField.stop();
 	}
 	this.remove = function(){
-		this.zombieHtml.remove();
+		this.zombieField.remove();
 	}
 }
